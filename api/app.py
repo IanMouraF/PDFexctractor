8dc1d03938e4d255d706790f4eaba45f4634174d
IanMouraF <ianmourafac@gmail.com>
2024-07-10 13:59:01 -0300

    tratamento de erro

--- api/app.py
+++ api/app.py
@@ -4,14 +4,13 @@ from flask import Flask, request, render_template, send_file
 import PyPDF2
 import zipfile
 import io
+import tempfile
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['EXTRACTED_FOLDER'] = 'extracted'
 
-# Crie as pastas se não existirem
+# Crie a pasta de uploads se não existir
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-os.makedirs(app.config['EXTRACTED_FOLDER'], exist_ok=True)
 
 def sanitize_filename(filename):
     # Remove caracteres inválidos por sublinhados
@@ -24,8 +23,7 @@ def sanitize_filename(filename):
     filename = filename.strip()
     return filename
 
-
-def extract_pages(input_pdf_path, output_dir):
+def extract_pages(input_pdf_path):
     extracted_files = []
     # Abra o arquivo PDF de entrada
     with open(input_pdf_path, 'rb') as input_pdf:
@@ -51,7 +49,7 @@ def extract_pages(input_pdf_path, output_dir):
             nome_representante = sanitize_filename(nome_representante)
 
             # Nome do novo arquivo PDF
-            output_pdf_path = os.path.join(output_dir, f"{nome_representante}_pagina_{i + 1}.pdf")
+            output_pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{nome_representante}_pagina_{i + 1}.pdf")
 
             # Salve o novo arquivo PDF com a página atual
             with open(output_pdf_path, 'wb') as output_pdf:
@@ -62,12 +60,13 @@ def extract_pages(input_pdf_path, output_dir):
 
     return extracted_files
 
-def create_zip(files, output_dir):
-    zip_path = os.path.join(output_dir, 'extracted_files.zip')
-    with zipfile.ZipFile(zip_path, 'w') as zipf:
+def create_zip(files):
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'w') as zipf:
         for file in files:
             zipf.write(file, os.path.basename(file))
-    return zip_path
+    zip_buffer.seek(0)
+    return zip_buffer
 
 @app.route('/')
 def index():
@@ -75,17 +74,25 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    files = request.files.getlist('pdf')
-    extracted_files = []
-    for file in files:
-        if file and file.filename.lower().endswith('.pdf'):
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
-            extracted_files.extend(extract_pages(file_path, app.config['EXTRACTED_FOLDER']))
+    try:
+        files = request.files.getlist('pdf')
+        extracted_files = []
+        
+        for file in files:
+            if file and file.filename.lower().endswith('.pdf'):
+                # Use um diretório temporário para salvar temporariamente os arquivos
+                temp_dir = tempfile.mkdtemp()
+                file_path = os.path.join(temp_dir, file.filename)
+                file.save(file_path)
+                extracted_files.extend(extract_pages(file_path))
+        
+        zip_buffer = create_zip(extracted_files)
+        return send_file(zip_buffer, as_attachment=True, attachment_filename='extracted_files.zip')
     
-    zip_path = create_zip(extracted_files, app.config['EXTRACTED_FOLDER'])
-    return send_file(zip_path, as_attachment=True)
+    except Exception as e:
+        print(f"Error during file upload: {e}")
+        return "Internal Server Error", 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
